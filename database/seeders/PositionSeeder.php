b3e1ab51c0287d38d9aa0af8d3ab2d72fb5729ca
Ubuntu <ubuntu@ip-172-31-28-61.ap-northeast-1.compute.internal>
2023-06-06 03:32:45 +0000

    Positionテーブルにseederでデータ投入。Postionモデル作成

---
+++ database/seeders/PositionSeeder.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class PositionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('positions')->insert([
+            [
+                'name' => 'GK（ゴールキーパー）',
+            ],
+        ]);
+        DB::table('positions')->insert([
+            [
+                'name' => 'CB（センターバック）',
+            ],
+        ]);
+        DB::table('positions')->insert([
+            [
+                'name' => 'LB（レフトバック）',
+            ],
+        ]);
+        DB::table('positions')->insert([
+            [
+                'name' => 'RB（ライトバック）',
+            ],
+        ]);
+        DB::table('positions')->insert([
+            [
+                'name' => 'CDM（センターディフェンシブミッドフィールダー）',
+            ],
+        ]);
+        DB::table('positions')->insert([
+            [
+                'name' => 'CM（セントラルミッドフィールダー）',
+            ],
+        ]);
+        DB::table('positions')->insert([
+            [
+                'name' => 'RM（ライトミッドフィールダー）',
+            ],
+        ]);
+        DB::table('positions')->insert([
+            [
+                'name' => 'LM（レフトミッドフィールダー）',
+            ],
+        ]);
+        DB::table('positions')->insert([
+            [
+                'name' => 'RWB（ライトウィングバック）',
+            ],
+        ]);
+        DB::table('positions')->insert([
+            [
+                'name' => 'LWB（レフトウィングバック）',
+            ],
+        ]);
+        DB::table('positions')->insert([
+            [
+                'name' => 'RW（ライトウィンガー）',
+            ],
+        ]);
+        DB::table('positions')->insert([
+            [
+                'name' => 'LW（レフトウィンガー）',
+            ],
+        ]);
+        DB::table('positions')->insert([
+            [
+                'name' => 'CAM（セントラルアタッキングミッドフィールダー）',
+            ],
+        ]);
+        DB::table('positions')->insert([
+            [
+                'name' => 'CF（センターフォワード）',
+            ],
+        ]);
+        DB::table('positions')->insert([
+            [
+                'name' => 'ST（ストライカー））',
+            ],
+        ]);
+    }
+}
+
